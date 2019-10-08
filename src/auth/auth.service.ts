9556da1cbb6469be1b3ebe1035157bb409893be0
Steph <N/A>
2019-10-07 23:30:18 -0400

    auth dto validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@nestjs/common'
 
 import { UserRepository } from './user.repository'
 import { InjectRepository } from '@nestjs/typeorm'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    userRepository: UserRepository
+    private userRepository: UserRepository
   ) { }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto)
+  }
 }
